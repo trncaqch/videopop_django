f33609a648f733598138ce68af10deccdf66102d
unknown <2030449c@BO715-6-03.ad.dcs.gla.ac.uk>
2015-03-17 15:09:58 +0000

    created basic wireframe

--- videopop/videopop/settings.py
+++ videopop/videopop/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,6 +52,12 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'videopop.urls'
 
 WSGI_APPLICATION = 'videopop.wsgi.application'
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
 
 
 # Database
--- videopop/videopop/urls.py
+++ videopop/videopop/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from app import views, gameViews, scoreViews, resultViews, userViews
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'videopop.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^play', gameViews.play, name='play'),
+    url(r'^scores', scoreViews.scores, name = 'scores'),
+    url(r'^results', resultViews.results, name = 'results'),
+    url(r'^user/(?P<user_name>[\w\-]+)/$', userViews.userProfile,
+        name = 'profile')
 )
