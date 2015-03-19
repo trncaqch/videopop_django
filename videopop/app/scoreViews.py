721149fb58dbf46330f442fa00a6b2b0b5f7721f
unknown <2030449c@BO715-8-05.ad.dcs.gla.ac.uk>
2015-03-19 19:29:04 +0000

    database 2.0

--- videopop/app/scoreViews.py
+++ videopop/app/scoreViews.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Score
 
 def scores(request):
-    return render(request, 'app/scores.html')
+    score_list = Score.objects.order_by('-score')
+    context_dict = { 'scores' : score_list}
+    return render(request, 'app/scores.html', context_dict)
