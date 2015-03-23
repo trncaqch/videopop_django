5224249772a0c2901cd33ce890fd7df8c4f1d9ec
unknown <2030449c@BO706-41.ad.dcs.gla.ac.uk>
2015-03-23 16:38:06 +0000

    some html stuff

--- videopop/app/gameViews.py
+++ videopop/app/gameViews.py
@@ -4,17 +4,13 @@ from models import Video, Score
 from django.http import HttpResponse, HttpResponseRedirect
 import time
 
-
-
 refreshes = -1
-oldanswer = ''
+old_answer = ''
 correctAnswers = 0
 vidStart = time.time()
 gameStart = time.time()
 score = 0
 blacklist = []
-
-
 
 def play(request):
     # fields
@@ -26,19 +22,13 @@ def play(request):
     global correctAnswers
     global gameStart
     global score
-    timeLeft = 30 - (int(time.time()) - int(gameStart))
+    time_left = 30 - (int(time.time()) - int(gameStart))
 
     # if its the first time the game is started
+    # this should definitely be a function
     if (refreshes < 0) or (int(time.time() - vidStart) > 5):
-        print "restart"
-        refreshes = -1
-        oldanswer = ''
-        correctAnswers = 0
-        vidStart = time.time()
-        gameStart = time.time()
-        score = 0
-        blacklist = []
-        timeLeft = 30
+        restart()
+        time_left = 30
 
 
     if request.method == "POST":
@@ -48,7 +38,7 @@ def play(request):
             correctAnswers += 1
             score = score + points
         else:
-            timeLeft = timeLeft - int(points)
+            time_left = time_left - int(points)
 
     # if request.method == "GET":
 
@@ -67,15 +57,15 @@ def play(request):
 
     refreshes = refreshes + 1
     answer = play_list[0].correctAnswer
-    oldanswer = answer
+    old_answer = answer
     shuffle(play_list)
     context_dict = {"source": src,
                     "videos": play_list,
                     "answers": answer,
-                    "timeleft": timeLeft
+                    "timeleft": time_left
     }
 
-    if timeLeft <= 0:
+    if time_left <= 0:
         if request.user.is_authenticated():
             add_score(request.user, score, correctAnswers, refreshes)
         gameStart = time.time()
@@ -84,13 +74,26 @@ def play(request):
         return HttpResponseRedirect('/vidpop/results')
 
     print int(time.time() - vidStart)
-    print timeLeft
+    print time_left
     vidStart = time.time()
     print score
     return render(request, 'app/game.html', context_dict)
 
 def restart():
-    pass
+    global refreshes
+    refreshes = -1
+    global oldanswer
+    oldanswer = ''
+    global correctAnswers
+    correctAnswers = 0
+    global vidStart
+    vidStart = time.time()
+    global gameStart
+    gameStart = time.time()
+    global score
+    score = 0
+    global blacklist
+    blacklist = []
 
 def add_score(user, score, correctAnswers, videosSeen):
     s = Score.objects.create(user=user)
