c18faf90e634aaf983cf90d59fbeeb0826da6f56
unknown <2030449c@BO706-12.ad.dcs.gla.ac.uk>
2015-03-27 14:55:03 +0000

    asd

--- videopop/app/forms.py
+++ videopop/app/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from app.models import UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -8,7 +7,3 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
