c13abed4fa7adacb110641d0218fc4a7ddbbe8db
Oliver Hawes <oliver@olihawes.co.uk>
2015-03-30 16:40:01 +0100

    Removed duplicates in answers

---
+++ videopop/app/migrations/0003_auto_20150330_1555.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0002_auto_20150327_0322'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='userprofile',
+            name='user',
+        ),
+        migrations.DeleteModel(
+            name='UserProfile',
+        ),
+    ]
